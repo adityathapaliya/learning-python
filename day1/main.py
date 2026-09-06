a2d09864b52efe49cbee9bb1fd387b6e1cdd2739
aditya thapaliya <adityathapaliya90@gmail.com>
2026-09-06 08:23:34 +0545

    learned basic of pythons

---
+++ day1/main.py
@@ -0,0 +1,66 @@
+# print('hello world')
+
+# s = "yo"
+# print(s)
+
+# s = 10
+# age = 25
+# city = "dharan"
+# print(s, age, city)
+
+# x ,y = input("Enter a value").split()
+
+# print(x)
+
+# a = 10
+# b = 10
+# print(a is b)
+
+# import keyword
+# print("The list of keywords are : ")
+# print(keyword.kwlist)
+
+# a = "Aditya thapaliya"
+# print(a)
+# print(type(a))
+
+# print(a[1])
+# print(a[-1])
+
+# b="harshit"
+# l = ["aditya", 91, 87, b, 87, 67 ]
+
+# print (l)
+# print(l[0])
+# print(l[-3])
+# print(l[0:3])
+
+
+# k = 12
+# t = (k,)
+
+# print(type(t))
+
+# s = {1,3,3,5,56,756,75,67,}
+# print(type(s))
+
+# d = {"Aditya":87, "harshit": 86, "alson":86 }
+
+# print(d["Aditya"])
+
+# l = [32,423,4,23,42,34,23,57,658,67,5]
+# print(len(l))
+# print (l)
+# l = list((435,345,34,5658,67,879,))
+# print(l)
+# print(len(l))
+
+# thislist = ["apple", "banana", "cherry"]
+# thislist[1:3] = ["watermelon"]
+# print(thislist)
+
+# thislist = ["apple", "banana", "cherry"]
+# tropical = ["mango", "pineapple", "papaya"]
+# thislist.extend(tropical)
+# print(thislist)
+
