1d54b7a17c42c4d6c07a0c823f181c8204abd721
aditya thapaliya <adityathapaliya90@gmail.com>
2026-09-25 08:53:17 +0545

    did some question doe to learn if else statement better

--- day3/main.py
+++ day3/main.py
@@ -1,31 +1,57 @@
-#Students mark checking system
-student_1 = {"name":"aditya","marks" :[40,88,96,99,83]}
-student_2 = {"name":"girish","marks" :[68,79,45,78,85]}
-student_3 = {"name":"sudip","marks" :[70,76,54,98,88]}
-student_4 = {"name":"rushan","marks" :[57,78,56,99,93]}
-student_5 = {"name":"alson","marks" :[50,88,86,79,63]}
-marks = student_1["marks"]
-marks.sort()
-avg_marks= sum(marks)/5
+# #Students mark checking system
+# student_1 = {"name":"aditya","marks" :[40,88,96,99,83]}
+# student_2 = {"name":"girish","marks" :[68,79,45,78,85]}
+# student_3 = {"name":"sudip","marks" :[70,76,54,98,88]}
+# student_4 = {"name":"rushan","marks" :[57,78,56,99,93]}
+# student_5 = {"name":"alson","marks" :[50,88,86,79,63]}
+# marks = student_1["marks"]
+# marks.sort()
+# avg_marks= sum(marks)/5
 
-if marks[0] < 40:
-    print('fail')
+# if marks[0] < 40:
+#     print('fail')
 
-else:
-    print("student_name =",student_1["name"])
-    print("average_marks = ", avg_marks)
+# else:
+#     print("student_name =",student_1["name"])
+#     print("average_marks = ", avg_marks)
 
-    if avg_marks > 100:
-        print("number is wrong")
-    elif avg_marks  >= 80:
-        print("Excellent")
-    elif avg_marks  >= 60:
-       print("good")
-    elif avg_marks  >= 40:
-       print("passed")
-    else:
-        print("invalid input")
-#############################################################
+#     if avg_marks > 100:
+#         print("number is wrong")
+#     elif avg_marks  >= 80:
+#         print("Excellent")
+#     elif avg_marks  >= 60:
+#        print("good")
+#     elif avg_marks  >= 40:
+#        print("passed")
+#     else:
+#         print("invalid input")
+# #############################################################
+
+
+# 
+# games = {
+#     "favorite": "MLBB",
+#     "played": ["MLBB", "Minecraft", "Valorant"],
+#     "genres": ("MOBA", "Sandbox", "FPS"),
+#     "friends_games": {"MLBB", "Free Fire", "Minecraft"}
+# }
+# print("Mlbb")
+# if "MLBB" in games["played"] :
+#     print("i have played it")
+# if "Valorant" in games["friends_games"]:
+#     print("have played it ")
+# if "Minecraft" in games["played"] and "Minecraft" in games["friends_games"]:
+#     print("both have it") 
+# if games["favorite"] in games["friends_games"]:
+#     print("we can play together")
+# else:
+#     print("let play some thing else together")
+################################################################
+
+
+
+
+
 
 
 
